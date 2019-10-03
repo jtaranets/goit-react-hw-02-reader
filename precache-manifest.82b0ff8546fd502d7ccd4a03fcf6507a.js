9d94689c4593bdbf9926416173898447a58f9749
Julia Taranets <jtaranets@gmail.com>
2019-10-03 20:41:51 +0300

    Updates

---
+++ precache-manifest.82b0ff8546fd502d7ccd4a03fcf6507a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57aa600680df9b699511e5d44c0d10b1",
+    "url": "/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "63dc3d65fa0b5766e9dd",
+    "url": "/goit-react-hw-02-reader/static/css/main.0e83c4f3.chunk.css"
+  },
+  {
+    "revision": "f40a2899392e5a74a0c4",
+    "url": "/goit-react-hw-02-reader/static/js/2.df0e0984.chunk.js"
+  },
+  {
+    "revision": "63dc3d65fa0b5766e9dd",
+    "url": "/goit-react-hw-02-reader/static/js/main.e83c78e8.chunk.js"
+  },
+  {
+    "revision": "22bad9f5e9ec4c3919a1",
+    "url": "/goit-react-hw-02-reader/static/js/runtime-main.ef339caf.js"
+  }
+]);
